3a895a180df040779a082784fef5c50c82d38f86
Stephen Farnsworth <robotoman@gmail.com>
2021-11-27 19:20:20 -0700

    Save work

--- run_docker.sh
+++ run_docker.sh
@@ -5,5 +5,5 @@ docker run \
   -v /Users/ilektron/Documents/mcserver:/data:rw \
   -p 25565:25565 \
   -p 25575:25575 \
--i ilektron/minecraft-papermc-server:latest
+  -i ilektron/minecraft-papermc-server:latest
 docker attach mcserver
