e9ee1d3145c21f061c069019e21e5c51429719a9
Marc Toensing <marc.toensing@gmail.com>
2022-07-21 16:47:12 +0200

    beta run

--- run_docker_beta.sh
+++ run_docker_beta.sh
@@ -5,5 +5,5 @@ docker run \
   -v /Users/mtoe/Documents/mcserver:/data:rw \
   -p 25565:25565 \
   -p 25575:25575 \
--i marctv/minecraft-papermc-server:latest
+-i marctv/minecraft-papermc-server:beta
 docker attach mcserver
