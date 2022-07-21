1a58277d3733be0e05854b132083d2be05f41347
Marc Toensing <marc.toensing@gmail.com>
2022-07-21 18:50:07 +0200

    beta start scripts

--- run_docker_bash.sh
+++ run_docker_bash.sh
@@ -3,5 +3,5 @@ docker run \
   --name mcserver \
   --entrypoint=/bin/bash \
   -v /Users/mtoe/Documents/mcserver:/data:rw \
-  -p 25565:25565 \
--i marctv/minecraft-papermc-server:latest
+  -p 25566:25565 \
+-i marctv/minecraft-papermc-server:beta
--- run_docker_beta.sh
+++ run_docker_beta.sh
@@ -2,8 +2,7 @@ docker run \
   --rm \
   --name mcserver \
   -e MEMORYSIZE='1G' \
-  -v /Users/mtoe/Documents/mcserver:/data:rw \
-  -p 25565:25565 \
-  -p 25575:25575 \
+  -v /volume2/SSD/mcserver-beta:/data:rw \
+  -p 25566:25565 \
 -i marctv/minecraft-papermc-server:beta
 docker attach mcserver
