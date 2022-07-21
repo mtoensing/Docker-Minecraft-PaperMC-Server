feb3d6f7fe83741f31aad909e1cde86e1413acd4
Michael Reiner <michael.reiner@oestu-stettin.at>
2022-07-21 13:49:22 +0200

    readded output of paper version and build

--- getpaperserver.sh
+++ getpaperserver.sh
@@ -3,6 +3,8 @@
 PAPERMC_VERSION=$1
 LATEST_BUILD=$(curl -s "https://papermc.io/api/v2/projects/paper/versions/${PAPERMC_VERSION}" | jq '.builds[-1]')
 LATEST_DOWNLOAD=$(curl -s "https://papermc.io/api/v2/projects/paper/versions/${PAPERMC_VERSION}/builds/${LATEST_BUILD}" | jq '.downloads.application.name' -r)
-
+echo -----------------
+echo $PAPERMC_VERSION#$LATEST_BUILD
+echo -----------------
 PAPERMC_DOWNLOAD_URL="https://papermc.io/api/v2/projects/paper/versions/${PAPERMC_VERSION}/builds/${LATEST_BUILD}/downloads/${LATEST_DOWNLOAD}"
 curl -s -o paperclip.jar ${PAPERMC_DOWNLOAD_URL}
